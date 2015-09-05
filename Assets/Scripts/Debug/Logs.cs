a3ae46b8214b079d4aac1ecd52152ccf5f1899a2
hom999 <hom999.to@gmail.com>
2015-09-05 15:42:35 +0900

    Added custum og classes

---
+++ Assets/Scripts/Debug/Logs.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logs : MonoBehaviour 
+{
+	// -------------------------------------------------- singleton
+    private static Logs mInstance;
+    private Logs () //private constructor
+	{
+		Debug.Log("Create SampleSingleton instance.");
+    }
+    public static Logs Instance
+	{
+        get {
+            if( mInstance == null ) mInstance = new Logs();
+            return mInstance;
+        }
+	}
+	// -------------------------------------------------- >>
+	
+	private bool isDebugging;
+	public bool Isdebugging
+	{
+		set { this.isDebugging = value; }
+		get { return this.isDebugging; }
+	}
+	
+	public void p(msg)
+	{
+		log(msg, "MYLOGS ------------> ");
+	}
+	
+	public void l(string msg)
+	{
+		log(msg, "MYLOGS ---> ");
+	}
+	
+	public void log(string msg, string tag){
+		if(!isDebugging)
+		{
+			return;
+		}
+		Debug.log(tag + msg);
+	}
+	
+	
+}
+
